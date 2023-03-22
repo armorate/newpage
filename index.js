8330b8a15cd9c5e01a4b6165b8a8eeb759a707be
armorate <blinder404@gmail.com>
2023-03-22 23:30:08 +0530

    Objects & referencing their properties

--- index.js
+++ index.js
@@ -3,5 +3,16 @@ let lastName = 'Rastogi';
 console.log(firstName, lastName);
 
 const birthYear = 1992;
-birthYear = 1995;
+//birthYear = 1995; /*this line would throw an error and after it execution will stop*/
 console.log(birthYear);
+
+// Objects
+let personName = { 
+    firstName: 'Ramesh', 
+    lastName: 'Gopalachari'
+    };
+    /*there are two ways to reference objects properties 1. Dot Notation 2. Bracker Notation*/
+console.log(
+    personName.lastName, /* Dot Notation*/
+    personName['firstName'] /* Bracket Notation*/
+    );
