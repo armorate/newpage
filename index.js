eb13c4a8813fff91f8cc702a16713a37d47282cb
armorate <blinder404@gmail.com>
2023-05-08 23:28:20 +0530

    Logical Operator with non-boolean values

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-// Logical Operator with boolean values (does the AND, OR and NOR operations)
+// Logical Operator with Non-boolean values
 
-let attendence = true;
-let percentage = false;
+// if we compare a boolean false and a non-boolean value, then we will get the non-boolean value as true*.
+// Some non-boolean values are considered false in javascript which are :-  undefined, null, 0, false, '', NaN
 
-//AND operator
-let result = attendence && percentage;
-console.log(result);
+let op1 = false || true;
+console.log(op1);
 
-// OR operator
-let promoted = attendence || percentage;
-console.log(promoted);
+let op2 = false || 4;
+console.log(op2);
 
-// NOR operator
-let failed = !result;
-console.log(failed);
+let op3 = false || "tej";
+console.log(op3);
+
+let op4 = 098 || true;
+console.log(op4);
+
+let op5 = null || true;
+console.log(op5);
+
+let op6 = false || NaN;
+console.log(op6);
+// in op6 it returns NaN, because it starts from left and skips false because its false and returns NaN.
