6aeade6742cdcffd1c3e54b6c0ddca161129ed44
armorate <blinder404@gmail.com>
2023-05-11 01:00:27 +0530

    swapping variables Programme

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-// Bitwise Operators => these are used to do operations between binary values.
+// Swapping Variables
 
-// 1 = 00000001
-// 2 = 00000010
+let a = 56;
+let b = "you";
 
-// Bitwise OR ( | )
-console.log(2 | 1);
-// here it will compare both the binary values bitwise at each index and return the true value for that index in output if atleast one of the value is true.
+let c = a;
+a = b;
+b = c;
 
-// Bitwise AND ( & )
-console.log(1 & 2);
-// here it will compare both the binary values at each bit by index and return true only when both the values at the same index are also true.
+console.log(a);
+console.log(b);
