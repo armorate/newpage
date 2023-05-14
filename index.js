a9c82b5643e9bac09ce1aeac5d2107ac49d03945
armorate <blinder404@gmail.com>
2023-05-15 03:06:13 +0530

    conitue and break keyword in loops

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Loops => there many kinds of loops in js.
 
-// for-of loop
+// Continue and Break keywords
 
-// this loop used the display elements of an array.
-
-let stuff = [45, 78, 0.7, 53];
-
-for (let key of stuff) console.log(key);
-
-// In for-of loop, the key is generated after each iteration returns the value of the element instead of propertie/metadata.
-
-// For-of loops are only used for arrays and use of for-in loop is not recommended for arrays.
+for (i = 3; i <= 9; i++) {
+  if (i === 4) {
+    continue; // continue skips the current iteration.
+  }
+  if (i % 4 === 0) {
+    break; // break stops the loop.
+  }
+  console.log(i);
+}
