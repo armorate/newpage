7c158a6814382c2c572bffc8047ff990d9241b22
armorate <blinder404@gmail.com>
2023-05-15 04:15:23 +0530

    Ex- Programme to fizzBuzz

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-// write a function to find oreantation of image.
+// write a function to fizzbuzz
+// Divisible by 3 return fizz
+// Divisible by 5 return buzz
+// Divisible by both 3 and 5 return fizzbuzz
+// Divisible by none then return the number
+// If not a number then return Not A Number
 
-let op = imageOr(448, 574);
-console.log("image is " + op);
+let op = fizzBuzz(45000);
+console.log(op);
 
-function imageOr(width, height) {
-  if (width > height) return "landscape.";
-  else if (width < height) return "vertical.";
-  else return "square.";
+function fizzBuzz(number) {
+  if (isNaN(number)) return "Not A Number";
+  else if (number % 15 === 0) return "fizzBuzz";
+  else if (number % 3 === 0) return "fizz";
+  else if (number % 5 === 0) return "Buzz";
+  else return number;
 }
