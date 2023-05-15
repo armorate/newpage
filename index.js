130b9a5dee3a5d5901ee2d63d6d8aa3a61dd6707
armorate <blinder404@gmail.com>
2023-05-16 04:25:20 +0530

    Ex- Count truthy values in an array

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-// Write a programme to print numbers and their nature(odd/even).
+// Write a programme to count truthy values in an array.
 
-printNumbers(5);
+let array = [false, "fd", 45, null, 0, true, "number", NaN, 99, 38, " ", ""];
 
-function printNumbers(toNum) {
-  for (i = 0; i <= toNum; i++) {
-    let type = i % 2 === 0 ? "Even" : "Odd";
-    console.log(i + " " + type);
+countTruthy(array);
+
+function countTruthy(array) {
+  let i = 0;
+  for (let key of array) {
+    if (key) i++;
   }
+  console.log(i);
 }
