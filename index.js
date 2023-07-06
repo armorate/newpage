9d98ae1bf001cddc6edc2414b6a3ee99e4e854bd
armorate <blinder404@gmail.com>
2023-07-06 21:51:45 +0530

    Ex- Programme to log stars in a given number

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-// Write a programme for a grade system for student's marks(taking marks in object data type).
+// Write a programme to log stars in a given number.
 
-result = {
-  Maths: 34,
-  Science: 63,
-  English: 93,
-  SocialScience: 79,
-  Spanish: 4,
-};
-
-console.log(gradeSys(result));
-
-function gradeSys(object) {
-  let i = 0;
-  for (let key in object) {
-    i += object[key];
+logStars(17);
+function logStars(num) {
+  for (let i = 1; i <= num; i++) {
+    let bar = "";
+    for (let j = 1; j <= i; j++) bar += "*";
+    console.log(bar);
   }
-  console.log("Total Marks : " + i);
-  let j = i / Object.keys(object).length;
-  console.log("Average : " + j);
-  if (j < 40) return "F";
-  if (j < 60) return "C";
-  if (j < 80) return "B";
-  else if (j <= 100) return "A";
-  else return "enter valid result data!";
 }
