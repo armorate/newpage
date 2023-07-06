c43198def1cb02db727b6702a2d6f894995d886f
armorate <blinder404@gmail.com>
2023-07-06 21:53:51 +0530

    Ex- Programme to log prime numbers

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-// Write a programme to log stars in a given number.
+// Write a programme to log prime numbers till given number.
 
-logStars(17);
-function logStars(num) {
-  for (let i = 1; i <= num; i++) {
-    let bar = "";
-    for (let j = 1; j <= i; j++) bar += "*";
-    console.log(bar);
+logPrime(7);
+
+function logPrime(num) {
+  for (let i = 2; i <= num; i++) {
+    let isPrime = true;
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) console.log(i);
   }
 }
