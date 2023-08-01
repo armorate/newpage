3bab64aa18cc17cd930d8e8d2db0a50c9e1f1fe1
armorate <blinder404@gmail.com>
2023-08-01 23:26:40 +0530

    value & reference types

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-// Object Stuff
-// initializing a variable using const does'nt mean it's content can not be modified, it means that that specific variable name can not be re-assigned.
+// Value vs Reference types
+// in primitive/value type the memory address of variable and data is same unlike reference type where the memort addresses of variable and data are different.
+// for Primitive/Value type
+let x = 20;
 
-const rectangle = { height: 30 };
+function increment(num) {
+  num++;
+}
 
-rectangle.width = 10; // to add
-rectangle.area = function () {
-  let i = rectangle.height * rectangle.width;
-  console.log(i);
-};
+increment(x); // here in the function, parameter num is assigned a copy of the value of x, which is now independent of x's actual value
+console.log(x); // Primitive/value types are copied by their value
 
-delete rectangle.area; //  to delete
+// for Reference type
+let obj1 = { y: 30 };
 
-console.log(rectangle);
+function incrementobj(obj) {
+  obj.y++;
+}
+
+incrementobj(obj1); // here in the function, parameter obj is assigned the reference of obj1, which means they are holding the same mamery address value
+console.log(obj1.y); // Reference/Object type are copied by their reference (address in the memory)
