8bbbb651c0227b191ca504c046bca64457bf9104
armorate <blinder404@gmail.com>
2023-08-09 04:56:34 +0530

    Some built-in Objects- Math, String, Date

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-// Cloning an object
+// Some of the built-in Objects in JavaScript
 
-const person = {
-  name: "Sam",
-  age: 33,
-  adult: function () {
-    return person.age >= 18 ? "True" : "False";
-  },
-};
+// Math - The Math object contains static properties and methods for mathematical constants and functions.
 
-// using for-in loop
-// const obj1 = {};
-// for (key in person) {
-//   obj1[key] = person[key];
-// }
+console.log("Pi = " + Math.PI);
+let x1 = Math.sqrt(7);
+console.log(`SQRT =  ` + x1);
+let x2 = Math.floor(5.126);
+console.log(x2);
 
-// using assign
-// const obj1 = { sex: "male" };
-// Object.assign(obj1, person);
-// or
-// const obj1 = Object.assign({}, person);
+// String - The String object is used to represent and manipulate a sequence of characters.
 
-// using Spread operator(shortest way)
-const obj1 = { ...person };
+// Strings can be created as primitives, from string literals, or as objects, using the String() constructor//
 
-console.log(obj1);
-console.log(person.adult());
+// String Primitives
+const x11 = "Everest Is big.";
+// String Objects
+const x12 = new String("Really high!");
+
+const x13 = x11 + " \n" + x12;
+console.log(x13);
+const x14 = x11.charAt(12);
+console.log(x14);
+console.log(x12.toUpperCase());
+
+// Date - JavaScript Date objects represent a single moment in time in a platform-independent format.
+
+let x21 = new Date(2002, 9, 23, 11, 31);
+console.log(x21);
+console.log(x21.getUTCDate());
