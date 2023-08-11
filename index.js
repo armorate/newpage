aa8c530e1ab4c4f7254b14598b2a13e580647e06
armorate <blinder404@gmail.com>
2023-08-12 02:36:17 +0530

    Ex- Create an object using factory and constructor functions

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-// Make an address object and log it's properties and values(STREET, CITY, ZIPCODE).
+// Create an address object using factory function and constructor function
 
-const address = { city: "Memphis", street: 45, zipcode: 43994 };
+// using factory function
+function createAddress1(name, zipCode) {
+  return {
+    name,
+    zipCode,
+    Country: "USA",
+  };
+}
 
+const address1 = createAddress1("Samuel Gibson", 33824);
+
+// using constructor function
+function CreateAddress2(zipCode) {
+  (this.city = "london"), (this.zipCode = zipCode), (this.Country = "UK");
+}
+
+const address2 = new CreateAddress2(11255);
+
+// a function to log object's properties and values
 function showAddress(obj) {
   for (let key in obj) {
     console.log(key, obj[key]);
   }
 }
 
-showAddress(address);
+showAddress(address1);
+showAddress(address2);
