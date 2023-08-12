5396da7dc586e63ff9130076dde29f674ba6bd36
armorate <blinder404@gmail.com>
2023-08-13 02:36:54 +0530

    Ex- Create a blogspot object using object literal syntax

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
-// Create two functions, one to check if two objects have same elements and second to check if two objects are referencing to the same object.
+// Create a blogspot object using object literal syntax with following properties:
+// title, body, author, views, comments(author, body), isLive
 
-// using constructor function
-function CreateAddress(name, zipCode, country) {
-  this.name = name;
-  this.zipCode = zipCode;
-  this.country = country;
-}
+let blogspot = {
+  title: "Health",
+  body: "Berries good, sugar bad",
+  author: "armorate",
+  views: 334,
+  comments: [
+    { author: "a", body: "b" },
+    { author: "c", body: "d" },
+  ],
+  isLive: false,
+};
 
-const address1 = new CreateAddress("a", "b", "c");
-const address2 = new CreateAddress("a", "b", "c");
-const address3 = address1;
-
-// to check if both objects have same properties
-function areEqual(address1, address2) {
-  return (
-    address1.name == address2.name &&
-    address1.zipCode == address2.zipCode &&
-    address1.country == address2.country
-  );
-}
-
-// to check if both objects are referencing the same object
-function areSame(add1, add2) {
-  return add1 === add2;
-}
-
-console.log(areEqual(address1, address2));
-
-console.log(areSame(address1, address2));
-console.log(areSame(address1, address3));
+console.log(blogspot);
