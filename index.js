ff7321373dddcdf8d2ee064f12930d1eb4f29622
armorate <blinder404@gmail.com>
2023-08-14 03:13:41 +0530

    Concatenating and slicing arrays

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-// Emptying an array.
+// Concatenating and slicing arrays.
 
-let num = [3, 5, 3, 44, 2];
-let another = num; // We need to reference the num variable to another variable so that after emtying the num variable it does'nt get automatically deleted by the garbage collector.
+const num1 = [3, 4, 5];
+const num2 = [6, 7];
 
-// Solution 1
-// num = [];
+// Concatenation
+const num3 = num1.concat(num2);
+console.log(num3);
 
-// Solution 2
-// num.length = 0;
-
-// Solution 3
-// num.splice(0, num.length);
-
-// Solution 4
-// while (num.length > 0) num.pop();
-
-console.log(num);
-console.log(another);
+// Slicing
+const num4 = num3.slice(2, 4);
+console.log(num4);
