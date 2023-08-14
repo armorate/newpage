4cfa70acf9951527e660b513afefb4f2cf948cbe
armorate <blinder404@gmail.com>
2023-08-15 04:35:15 +0530

    Spread Operator

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-// Concatenating and slicing arrays.
+// Spread operator.
 
 const num1 = [3, 4, 5];
 const num2 = [6, 7];
 
-// Concatenation
-const num3 = num1.concat(num2);
+// Concatenation using spread operator
+const num3 = [...num1, "a", ...num2];
 console.log(num3);
 
-// Slicing
-const num4 = num3.slice(2, 4);
-console.log(num4);
+const copy = [...num2];
+console.log(copy);
