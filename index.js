c7ec2d36c05f363ff0485c69dbe46339b78a31ea
armorate <blinder404@gmail.com>
2023-08-18 02:25:06 +0530

    filter() method

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-// checking elements of an array
+// filtering elements from an array
 
-const num = [4, 3, -6, 7, 5];
+const num = [4, 3, -6, 7, -2, 5];
 
-// every() - exits as soon as condition is false
-const allPositive = num.every(function (value) {
-  return value >= 0;
-});
+// filter() - returns filtered elements
+const filtered = num.filter((x) => x >= 0);
 
-console.log(allPositive);
-
-// some() - exits as soon as condition is true
-const atLeastOnePositive = num.some(function (value) {
-  return value >= 0;
-});
-
-console.log(atLeastOnePositive);
+console.log(filtered);
