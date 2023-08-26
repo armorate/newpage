83442c9756274515410d8c9e0f9be461c3db1341
armorate <blinder404@gmail.com>
2023-08-27 01:55:12 +0530

    Ex- return the largest element of an array

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-// Ex- Write a programme to make a function that counts occurence of an element inside an array.
+// Ex- Write a programme to return the largest element of an array.
 
 const num = [3, 4, 2, 6, 4, 7];
-
-function countOccurences(array, element) {
-  let occurence = 0;
-  for (let x of array) if (x === element) occurence++;
-  return occurence;
-}
-
 console.log(num);
 
-console.log(countOccurences(num, 7));
+function getMaxValue(array) {
+  if (array.length === 0) return undefined;
+  let element = array[0];
+  for (let x of array) if (element < x) element = x;
+  return element;
+}
+
+console.log(getMaxValue(num));
+
+
+// using reduce() method
+function getMax(arr){
+  if(arr.length === 0) return undefined;
+  return arr.reduce((a,b)=> (a > b) ? a : b);
+}
+
+console.log(getMax(num));
