289853c7adef6af738203be323e1ed5261bbd2ad
armorate <blinder404@gmail.com>
2023-08-29 04:31:22 +0530

    Hoisting

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// Ex- Write a programme to filter movies releassed after 2000 and then sorted by their rating in descending order and return only the title of those movies.
+// Hoisting - hoisting is process that happens when we run the javascript code, at the runtime the function declarations get moved to the top of the program and are executed first.
 
-const movies = [
-  {title : 'd', year: 2005, rating: 4.0},
-  {title : 'g', year: 1995, rating: 4.9},
-  {title : 'j', year: 2004, rating: 4.6},
-  {title : 'a', year: 2012, rating: 2.5},
-  {title : 'r', year: 2018, rating: 4.5},
-];
+// function declaration
 
-const output = movies
-  .filter(m => m.year >= 2000 && m.rating >= 4.0)
-  .sort((a,b) => a.rating - b.rating)
-  .reverse()
-  .map(m => m.title)
+hello();
 
-console.log(output);
+function hello(){
+  console.log('hello');
+}
+
+// function expression
+
+hola();
+
+let hola = function() {
+  console.log('hola');
+}
