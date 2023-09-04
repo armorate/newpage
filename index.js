a53283f053bff2b9b5f00606c6e36fb19f1b493a
armorate <blinder404@gmail.com>
2023-09-05 01:16:11 +0530

    Ex- sum of arguments

--- index.js
+++ index.js
@@ -1,41 +1,15 @@
-// Changing 'this'
+// Write a programme to sum arguments and sum array as arguments.
 
-// there are many ways to change what this refers to inside an mehtods function, the preferred way is using arraw function.
-
-// using another varible
-const fun = {
-  name : 'red',
-  tags : ['a', 'b', 'c'],
-  showtags() {
-    let that = this;  // here that refers to what this is referring now
-    this.tags.forEach(function(tags) {
-      console.log(that.name, tags);
-      }
-    )
-  }
-};
-
-fun.showtags();
-
-// using bind method
-const fun1 = {
-  name : 'blue',
-  tags : ['a','b','c'],
-  showtags() {
-    this.tags.forEach( function(tags) {
-      console.log(this.name, tags);
-    }.bind(this));
-  }
+function sum(...items) {
+  return items.reduce((a,b) => a + b);
 }
 
-fun1.showtags();
+console.log(sum(3,4,55,4));
 
-// using arraw function
-const fun2 = {
-  name : 'green',
-  tags : ['a', 'b','c'],
-  showtags() {
-    this.tags.forEach(tags => console.log(this.name, tags)) } // inside an arrow function this refers to the inherited value of this from parent object
+function sumArray() {
+  if(arguments.length == 1 && Array.isArray(arguments[0]));
+  arguments = [...arguments[0]];
+  return arguments.reduce((a,b) => a + b);
 }
 
-fun2.showtags();
+console.log(sumArray([3,2,5]));
