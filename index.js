fb1d7b9a485336533422277f7457cb4b79ddc042
armorate <blinder404@gmail.com>
2023-09-12 04:47:50 +0530

    Ex- largest prime factor of a number

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-// ProjectEuler qid-2 - By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+// ProjectEuler qid-3 - What is the largest prime factor of the number 6000851475143
 
-function logFibSum(num) {
+function getPrimeFactors(num) {
 	let x;
-	let array = [1,2];
-	for(x = 1; x < 100; x++)
-	{	
-		if(array[x] >= num)
+	let a;
+	let array = [];
+	for(x = Math.ceil(num / 839); x >= 2; x--)	// 839 is seconcd largest factor of num, here it is used to shorten the loop, otherwise it will take hours to compile.
+	// for(x = 2; x < 5000)
+	{
+		if(num % x === 0)
 		{
-			array.pop(array[x]);
-			return array.reduce((a, b) => {if(b % 2 === 0){a += b;} return a;},0);
+			array.push(x);
 		}
-		array.push(array[x] + array[x - 1]);
 	}
-	return array.reduce((a, b) => a + b);
+	array.unshift(num / 71); // 71 is smallest factor of num, num/71 is largest.
+	a = array.length;
+	let y;
+	let z;
+	for(y = 0; y < a; y++)
+	{
+		for(z = 1; z < a; z++)
+		{
+			if(array[y] % array[z] === 0)
+			{
+				array.shift();
+			}
+		}
+	}
+	return array[0];
 }
 
-console.log(logFibSum(4000000));
+console.log(getPrimeFactors(600851475143));
