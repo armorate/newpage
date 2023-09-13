cbf356ee4f4d67f7422157aca738cd2a6529efdc
armorate <blinder404@gmail.com>
2023-09-13 05:50:12 +0530

    Ex- smallest multiple

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-// ProjectEuler qid-4 - Find the difference between the sum of the squares of the first one hundered natural numbers and the square of the sum.
+// ProjectEuler qid-5 - what is the smallest positive number that is completely divisible by all of the numbers from 1 to 20?
 
-function sumSquare(num) {
-	let x;
-	let y = 0;
-	let z = 0;
-	for(x = 1; x <= num; x++)
+
+function divisible(num) {
+	let x,y,z;
+	let a = 1;
+	let b = 0;
+	let c = 0;
+	// for(x = 1; x <= num; x++)
+	// {
+	// 	if(x == 5) {console.log(a);}
+	// 	if(a < x) { a *= x}
+	// 	else if(a % x !== 0) { a *= (x / (a % x));}
+	// }
+
+		for(x = 2; x < Number.MAX_SAFE_INTEGER; x++)
 	{
-		y += (x * x);
-		z += x;
+		for(y = 1; y <= num; y++)
+		{
+			if(x % y === 0) { b++;}
+			else { b = 0;}
+		}
+		if(b == num) {return x;}
 	}
-	return ((z * z) - y);
+	return a;
 }
 
-console.log(sumSquare(100));
+console.log(divisible(20));
