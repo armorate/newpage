4e53a605cf553c2e1a8ca5f6dfecc374d46cbfeb
armorate <vickykr1905@gmail.com>
2023-09-21 06:03:16 +0530

    Ex- Lattice Paths

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
-// ProjectEuler qid-14 - which stating number, under one million, produces the longest collatz sequence chain?
+// ProjectEuler qid-15 - Project Euler Problem 15.
 
-
-function largestCollatzNum(num) {
-    let tempNum, chainLength;
-    let currentLargestChain = 0;
-    let currentLargestChainNumber = 0;
-    if (num === 1) { return 1; }
-    for (let currentNumber = 2; currentNumber <= num; currentNumber++) {
-        chainLength = 1;
-        tempNum = currentNumber;
-        while (tempNum >= 2) {
-            if ((tempNum + 1) % 2 === 0 && tempNum >= 2) {
-                tempNum = (tempNum * 3) + 1;
-                chainLength++;
-            }
-            if (tempNum % 2 === 0) {
-                tempNum /= 2;
-                chainLength++;
-            }
-        }
-        if (chainLength > currentLargestChain) {
-            currentLargestChain = chainLength;
-            currentLargestChainNumber = currentNumber;
-        }
+function factorial(num) {
+    if (num === 1) {
+        return num;
     }
-    return currentLargestChainNumber;
+    else {
+        return num * factorial(num - 1);
+    }
 }
 
-console.log(largestCollatzNum(1000000));
+function routes(gridSize) {
+    let moves = gridSize * 2;
+    return (factorial(moves) / (factorial(gridSize) * factorial(moves - gridSize)));
+}
+
+console.log(routes(20));
